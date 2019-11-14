a7910d4ab4a594dc359c611ed28e7b3563b50294
zachary-peasley <zpman95@hotmail.com>
2019-11-14 09:57:48 -0600

    updated externalApi

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { useAuth0 } from "./react-auth0-spa";
-import PrivateRouteNarBar from "./Components/Utils/PrivateRoute";
+import PrivateRouteNarBar from "./Components/Utils/PrivateRouteNarBar";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Profile from './Components/Profile';
 import PrivateRoute from "./Components/PrivateRoutes";
+import ExternalApi from "./views/ExternalApi";
+
 function App() {
   const { loading } = useAuth0();
   if (loading) {
@@ -22,11 +24,13 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <PrivateRouteNarBar />
+        <Route path="/" exact />
         <PrivateRoute path="/profile" component={Profile} />
       </header>
       <Switch>
           <Route path="/" exact />
           <Route path="/profile" component={Profile} />
+          <PrivateRoute path="/external-api" component={ExternalApi} />
         </Switch>
       </BrowserRouter>
     </div>
