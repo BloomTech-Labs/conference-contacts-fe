c7c37b065a4c20c87a0b20a1977f41c0bdbc8f4b
zachary-peasley <zpman95@hotmail.com>
2019-11-14 09:44:29 -0600

    Updates, add privateRoutes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 import { useAuth0 } from "./react-auth0-spa";
-
+import PrivateRouteNarBar from "./Components/Utils/PrivateRoute";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Profile from './Components/Profile';
+import PrivateRoute from "./Components/PrivateRoutes";
 function App() {
   const { loading } = useAuth0();
   if (loading) {
@@ -13,22 +15,20 @@ function App() {
   }
   return (
     <div className="App">
+       <BrowserRouter>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-      
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        
-        >
-          Learn React
-        </a>
+        <PrivateRouteNarBar />
+        <PrivateRoute path="/profile" component={Profile} />
       </header>
+      <Switch>
+          <Route path="/" exact />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
