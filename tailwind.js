564848fde4e43d715df6cd37134c856143f01b10
macjabeth <macbethjonathan@gmail.com>
2019-11-19 17:04:55 -0500

    add purgecss plugin and configure dev/production scripts

--- tailwind.js
+++ tailwind.js
@@ -7,14 +7,12 @@ module.exports = {
       sm: '640px',
       md: '768px',
       lg: '1024px',
-      xl: '1280px',
+      xl: '1280px'
     },
     colors: {
       transparent: 'transparent',
-
       black: '#000',
       white: '#fff',
-
       gray: {
         100: '#f7fafc',
         200: '#edf2f7',
@@ -24,7 +22,7 @@ module.exports = {
         600: '#718096',
         700: '#4a5568',
         800: '#2d3748',
-        900: '#1a202c',
+        900: '#1a202c'
       },
       red: {
         100: '#fff5f5',
@@ -35,7 +33,7 @@ module.exports = {
         600: '#e53e3e',
         700: '#c53030',
         800: '#9b2c2c',
-        900: '#742a2a',
+        900: '#742a2a'
       },
       orange: {
         100: '#fffaf0',
@@ -46,7 +44,7 @@ module.exports = {
         600: '#dd6b20',
         700: '#c05621',
         800: '#9c4221',
-        900: '#7b341e',
+        900: '#7b341e'
       },
       yellow: {
         100: '#fffff0',
@@ -57,7 +55,7 @@ module.exports = {
         600: '#d69e2e',
         700: '#b7791f',
         800: '#975a16',
-        900: '#744210',
+        900: '#744210'
       },
       green: {
         100: '#f0fff4',
@@ -68,7 +66,7 @@ module.exports = {
         600: '#38a169',
         700: '#2f855a',
         800: '#276749',
-        900: '#22543d',
+        900: '#22543d'
       },
       teal: {
         100: '#e6fffa',
@@ -79,7 +77,7 @@ module.exports = {
         600: '#319795',
         700: '#2c7a7b',
         800: '#285e61',
-        900: '#234e52',
+        900: '#234e52'
       },
       blue: {
         100: '#ebf8ff',
@@ -90,7 +88,7 @@ module.exports = {
         600: '#3182ce',
         700: '#2b6cb0',
         800: '#2c5282',
-        900: '#2a4365',
+        900: '#2a4365'
       },
       indigo: {
         100: '#ebf4ff',
@@ -101,7 +99,7 @@ module.exports = {
         600: '#5a67d8',
         700: '#4c51bf',
         800: '#434190',
-        900: '#3c366b',
+        900: '#3c366b'
       },
       purple: {
         100: '#faf5ff',
@@ -112,7 +110,7 @@ module.exports = {
         600: '#805ad5',
         700: '#6b46c1',
         800: '#553c9a',
-        900: '#44337a',
+        900: '#44337a'
       },
       pink: {
         100: '#fff5f7',
@@ -123,8 +121,8 @@ module.exports = {
         600: '#d53f8c',
         700: '#b83280',
         800: '#97266d',
-        900: '#702459',
-      },
+        900: '#702459'
+      }
     },
     spacing: {
       px: '1px',
@@ -145,7 +143,7 @@ module.exports = {
       '40': '10rem',
       '48': '12rem',
       '56': '14rem',
-      '64': '16rem',
+      '64': '16rem'
     },
     backgroundColor: theme => theme('colors'),
     backgroundPosition: {
@@ -157,30 +155,30 @@ module.exports = {
       right: 'right',
       'right-bottom': 'right bottom',
       'right-top': 'right top',
-      top: 'top',
+      top: 'top'
     },
     backgroundSize: {
       auto: 'auto',
       cover: 'cover',
-      contain: 'contain',
+      contain: 'contain'
     },
     borderColor: theme => ({
       ...theme('colors'),
-      default: theme('colors.gray.300', 'currentColor'),
+      default: theme('colors.gray.300', 'currentColor')
     }),
     borderRadius: {
       none: '0',
       sm: '0.125rem',
       default: '0.25rem',
       lg: '0.5rem',
-      full: '9999px',
+      full: '9999px'
     },
     borderWidth: {
       default: '1px',
       '0': '0',
       '2': '2px',
       '4': '4px',
-      '8': '8px',
+      '8': '8px'
     },
     boxShadow: {
       default: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
@@ -190,7 +188,7 @@ module.exports = {
       '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
       inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
       outline: '0 0 0 3px rgba(66, 153, 225, 0.5)',
-      none: 'none',
+      none: 'none'
     },
     container: {},
     cursor: {
@@ -200,24 +198,24 @@ module.exports = {
       wait: 'wait',
       text: 'text',
       move: 'move',
-      'not-allowed': 'not-allowed',
+      'not-allowed': 'not-allowed'
     },
     fill: {
-      current: 'currentColor',
+      current: 'currentColor'
     },
     flex: {
       '1': '1 1 0%',
       auto: '1 1 auto',
       initial: '0 1 auto',
-      none: 'none',
+      none: 'none'
     },
     flexGrow: {
       '0': '0',
-      default: '1',
+      default: '1'
     },
     flexShrink: {
       '0': '0',
-      default: '1',
+      default: '1'
     },
     fontFamily: {
       sans: [
@@ -232,23 +230,10 @@ module.exports = {
         '"Apple Color Emoji"',
         '"Segoe UI Emoji"',
         '"Segoe UI Symbol"',
-        '"Noto Color Emoji"',
+        '"Noto Color Emoji"'
       ],
-      serif: [
-        'Georgia',
-        'Cambria',
-        '"Times New Roman"',
-        'Times',
-        'serif',
-      ],
-      mono: [
-        'Menlo',
-        'Monaco',
-        'Consolas',
-        '"Liberation Mono"',
-        '"Courier New"',
-        'monospace',
-      ],
+      serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+      mono: ['Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace']
     },
     fontSize: {
       xs: '0.75rem',
@@ -260,7 +245,7 @@ module.exports = {
       '3xl': '1.875rem',
       '4xl': '2.25rem',
       '5xl': '3rem',
-      '6xl': '4rem',
+      '6xl': '4rem'
     },
     fontWeight: {
       hairline: '100',
@@ -271,17 +256,17 @@ module.exports = {
       semibold: '600',
       bold: '700',
       extrabold: '800',
-      black: '900',
+      black: '900'
     },
     height: theme => ({
       auto: 'auto',
       ...theme('spacing'),
       full: '100%',
-      screen: '100vh',
+      screen: '100vh'
     }),
     inset: {
       '0': '0',
-      auto: 'auto',
+      auto: 'auto'
     },
     letterSpacing: {
       tighter: '-0.05em',
@@ -289,7 +274,7 @@ module.exports = {
       normal: '0',
       wide: '0.025em',
       wider: '0.05em',
-      widest: '0.1em',
+      widest: '0.1em'
     },
     lineHeight: {
       none: '1',
@@ -297,21 +282,21 @@ module.exports = {
       snug: '1.375',
       normal: '1.5',
       relaxed: '1.625',
-      loose: '2',
+      loose: '2'
     },
     listStyleType: {
       none: 'none',
       disc: 'disc',
-      decimal: 'decimal',
+      decimal: 'decimal'
     },
     margin: (theme, { negative }) => ({
       auto: 'auto',
       ...theme('spacing'),
-      ...negative(theme('spacing')),
+      ...negative(theme('spacing'))
     }),
     maxHeight: {
       full: '100%',
-      screen: '100vh',
+      screen: '100vh'
     },
     maxWidth: {
       xs: '20rem',
@@ -324,16 +309,16 @@ module.exports = {
       '4xl': '56rem',
       '5xl': '64rem',
       '6xl': '72rem',
-      full: '100%',
+      full: '100%'
     },
     minHeight: {
       '0': '0',
       full: '100%',
-      screen: '100vh',
+      screen: '100vh'
     },
     minWidth: {
       '0': '0',
-      full: '100%',
+      full: '100%'
     },
     objectPosition: {
       bottom: 'bottom',
@@ -344,14 +329,14 @@ module.exports = {
       right: 'right',
       'right-bottom': 'right bottom',
       'right-top': 'right top',
-      top: 'top',
+      top: 'top'
     },
     opacity: {
       '0': '0',
       '25': '0.25',
       '50': '0.5',
       '75': '0.75',
-      '100': '1',
+      '100': '1'
     },
     order: {
       first: '-9999',
@@ -368,12 +353,12 @@ module.exports = {
       '9': '9',
       '10': '10',
       '11': '11',
-      '12': '12',
+      '12': '12'
     },
     padding: theme => theme('spacing'),
     placeholderColor: theme => theme('colors'),
     stroke: {
-      current: 'currentColor',
+      current: 'currentColor'
     },
     textColor: theme => theme('colors'),
     width: theme => ({
@@ -406,7 +391,7 @@ module.exports = {
       '10/12': '83.333333%',
       '11/12': '91.666667%',
       full: '100%',
-      screen: '100vw',
+      screen: '100vw'
     }),
     zIndex: {
       auto: 'auto',
@@ -415,8 +400,8 @@ module.exports = {
       '20': '20',
       '30': '30',
       '40': '40',
-      '50': '50',
-    },
+      '50': '50'
+    }
   },
   variants: {
     accessibility: ['responsive', 'focus'],
@@ -484,8 +469,8 @@ module.exports = {
     whitespace: ['responsive'],
     width: ['responsive'],
     wordBreak: ['responsive'],
-    zIndex: ['responsive'],
+    zIndex: ['responsive']
   },
   corePlugins: {},
-  plugins: [],
-}
+  plugins: []
+};
