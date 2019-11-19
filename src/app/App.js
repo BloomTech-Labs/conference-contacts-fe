12a33dba48d9080f7e564869910f6669278c2568
zachary-peasley <zpman95@hotmail.com>
2019-11-19 17:05:36 -0600

    Added google analytics

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import ReactGA from 'react-ga'
 import { Route, Switch } from 'react-router-dom';
 import PrivateRoute from '../components/PrivateRoute';
 import { useAuth0 } from '../react-auth0-spa';
 import NavBar from '../components/NavBar';
 import Profile from '../features/profile/Profile';
-import Users from '../components/Users';
+// import Users from '../components/Users';
+
+
+function initizeAnalytics(){
+  ReactGA.initialize('UA-152876258-1')
+  ReactGA.pageview('/Homepage')
+}
+
 
 function App() {
+  initizeAnalytics()
   const { loading } = useAuth0();
 
   if (loading) {
@@ -18,10 +27,9 @@ function App() {
       <header>
         <NavBar />
       </header>
-
       <Switch>
         <Route path="/" exact render={() => <div>Home</div>} />
-        <Route path="/users" exact component={Users} />
+        {/* <Route path="/users" exact component={Users} /> */}
         <PrivateRoute path="/profile" component={Profile} />
       </Switch>
     </div>
