4afef3466cd9a7a86a5f92ed06c7395f967c4b94
zachary-peasley <zpman95@hotmail.com>
2019-12-09 16:59:29 -0600

    Added icons, styled the landing page

--- src/components/button.js
+++ src/components/button.js
@@ -1,7 +1,7 @@
 import tw from 'tailwind.macro';
 
 const Button = tw.button`
-bg-white font-bold rounded-full mt-6 py-4 px-8 shadow-lg uppercase tracking-wider
+bg-white font-bold rounded-full mt-6 py-4 px-8 shadow-lg uppercase tracking-wider bg-transparent hover:bg-blue-500 text-blue-700 hover:text-white
 `;
 
 export default Button;
--- src/containers/navbar.js
+++ src/containers/navbar.js
@@ -152,3 +152,4 @@ export default function NavBar() {
     </>
   );
 }
+
