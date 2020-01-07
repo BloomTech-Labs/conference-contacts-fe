d98747ed3a0ee1165d0d2e245eb4fb1735ae9167
Zach <zpman95@hotmail.com>
2020-01-07 12:20:15 -0600

    teampage color more pop

--- src/pages/teampage.js
+++ src/pages/teampage.js
@@ -11,7 +11,7 @@ export default function TeamPage() {
       <div className="flex flex-wrap">
         <div className="w-full md:w-1/3 px-2 mb-6">
           <div className="col-12 col-sm-6 col-lg-4">
-            <div className="grid container hovereffect hover-photo">
+            <div className="grid container hovereffect">
               
               <img
                 className="rounded-r-lg rounded-tl-lg ease img-size mx-auto"
@@ -58,7 +58,7 @@ export default function TeamPage() {
         </div>
         <div className="w-full md:w-1/3 px-2 mb-6">
           <div className="col-12 col-sm-6 col-lg-4">
-            <div className="grid container hovereffect hover-photo">
+            <div className="grid container hovereffect">
               <img
                 className="rounded-r-lg rounded-tl-lg img-size mx-auto"
                 src="https://i.ibb.co/yqZpTgq/T4-JUEB3-ME-UJ11-F1-S13-799843608d4e-512.png"
@@ -104,7 +104,7 @@ export default function TeamPage() {
         </div>
         <div className="w-full md:w-1/3 px-2 mb-4">
           <div className="col-12 col-sm-6 col-lg-4">
-            <div className="grid container hovereffect hover-photo">
+            <div className="grid container hovereffect">
               <img
                 className="rounded-r-lg rounded-tl-lg img-size  mx-auto"
                 src="https://i.ibb.co/WfqQd49/T4-JUEB3-ME-UL03-FJADB-b54ed5bc71f5-512.png"
@@ -149,7 +149,7 @@ export default function TeamPage() {
 
         <div className="w-full md:w-1/3 px-2 mb-4">
           <div className="col-12 col-sm-6 col-lg-4">
-            <div className="grid container hovereffect hover-photo">
+            <div className="grid container hovereffect">
               <img
                 className="rounded-r-lg rounded-tl-lg img-size mx-auto"
                 src="https://i.ibb.co/f0tsXRk/T4-JUEB3-ME-UJG8-RLWKG-3766d295008c-512.png"
@@ -195,7 +195,7 @@ export default function TeamPage() {
 
         <div className="w-full md:w-1/3 px-2 mb-4">
           <div className="col-12 col-sm-6 col-lg-4">
-            <div className="grid container hovereffect hover-photo">
+            <div className="grid container hovereffect">
               <img
                 className="rounded-r-lg rounded-tl-lg img-size mx-auto"
                 src="https://i.ibb.co/9bMB6H5/T4-JUEB3-ME-UHV013-MQF-3a9d77644b52-512.jpg"
@@ -240,7 +240,7 @@ export default function TeamPage() {
         </div>
         <div className="w-full md:w-1/3 px-2 mb-4">
           <div className="col-12 col-sm-6 col-lg-4">
-            <div className="grid container hovereffect hover-photo ">
+            <div className="grid container hovereffect">
               <img
                 className="rounded-r-lg rounded-tl-lg img-size mx-auto"
                 src="https://i.ibb.co/0Xfz8G3/T4-JUEB3-ME-UHF67-A9-T9-a3fb05aba2cd-512.jpg"
@@ -285,7 +285,7 @@ export default function TeamPage() {
         </div>
         <div className="w-full md:w-1/3 px-2 mb-4">
           <div className="col-12 col-sm-6 col-lg-4">
-            <div className="grid container hovereffect hover-photo">
+            <div className="grid container hovereffect">
               <img
                 className="rounded-r-lg rounded-tl-lg img-size mx-auto"
                 src="https://i.ibb.co/XDpNQJz/T4-JUEB3-ME-UHUR873-EV-d88881406d1d-512.jpg"
@@ -331,7 +331,7 @@ export default function TeamPage() {
 
         <div className="w-full md:w-1/3 px-2 mb-4">
           <div className="col-12 col-sm-6 col-lg-4">
-            <div className="grid container hovereffect hover-photo">
+            <div className="grid container hovereffect">
               <img
                 className="rounded-r-lg rounded-tl-lg img-size mx-auto"
                 src="https://i.ibb.co/D78c7Jp/T4-JUEB3-ME-UJWG6-UZG8-b02f88fab9ce-512.jpg"
@@ -376,7 +376,7 @@ export default function TeamPage() {
         </div>
         <div className="w-full md:w-1/3 px-2 mb-4">
           <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-            <div className="grid container hover-photo hovereffect">
+            <div className="grid container hovereffect">
               <img
                 className="img-responsive rounded-r-lg rounded-tl-lg img-size mx-auto"
                 src="https://i.ibb.co/vLwBkbX/T4-JUEB3-ME-UGVBY5-QK0-0322eb799fbf-512.jpg"
