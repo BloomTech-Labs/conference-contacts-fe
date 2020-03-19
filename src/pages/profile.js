2441c881cf982cc696eaed0e00d938f13bee5b7a
Erica L. Ingram <inquiries@aquoco.co>
2020-03-19 15:24:34 -0700

    changed qrcode color + adjusted profile pics to match in placement on web on home/profile pages

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -6,152 +6,169 @@ import Icon from '../components/icon';
 import BeatLoader from 'react-spinners/BeatLoader';
 
 const Profile = ({ location, navigate }) => {
-  const viewingContact = Boolean(location.state.userId);
-  const { loading, error, data } = useQuery(FETCH_USER_PROFILE, {
-    variables: { id: location.state.userId }
-  });
+	const viewingContact = Boolean(location.state.userId);
+	const { loading, error, data } = useQuery(FETCH_USER_PROFILE, {
+		variables: { id: location.state.userId }
+	});
 
-  const [deleteConnection, { loading: deleteLoading }] = useMutation(DELETE_CONNECTION, {
-    update(cache, { data: { deleteConnection: { connection } } }) {
-      const { user } = cache.readQuery({ query: GET_USER_CONNECTIONS });
-      const connections = user.connections.filter(c => c.id !== connection.id);
-      const pendingConnections = user.pendingConnections.filter(c => c.id !== connection.id);
-      cache.writeQuery({
-        query: GET_USER_CONNECTIONS,
-        data: {
-          user: location.state.status === 'PENDING'
-            ? { ...user, pendingConnections }
-            : { ...user, connections }
-        },
-      });
-    }
-  });
+	const [deleteConnection, { loading: deleteLoading }] = useMutation(DELETE_CONNECTION, {
+		update(
+			cache,
+			{
+				data: {
+					deleteConnection: { connection }
+				}
+			}
+		) {
+			const { user } = cache.readQuery({ query: GET_USER_CONNECTIONS });
+			const connections = user.connections.filter(c => c.id !== connection.id);
+			const pendingConnections = user.pendingConnections.filter(c => c.id !== connection.id);
+			cache.writeQuery({
+				query: GET_USER_CONNECTIONS,
+				data: {
+					user:
+						location.state.status === 'PENDING' ? { ...user, pendingConnections } : { ...user, connections }
+				}
+			});
+		}
+	});
 
-  if (loading || !data)
-    return (
-      <div className="flex justify-center h-screen items-center">
-        <BeatLoader size={35} loading={loading} color="#7B41FF" />
-      </div>
-    );
+	if (loading || !data)
+		return (
+			<div className="flex justify-center h-screen items-center">
+				<BeatLoader size={35} loading={loading} color="#7B41FF" />
+			</div>
+		);
 
-  if (error) return <p>There was an error: {error}</p>;
+	if (error) return <p>There was an error: {error}</p>;
 
-  const preferredContact = data.user.profile.find(field => field.preferredContact);
-  const contacts = preferredContact
-    ? data.user.profile.filter(field => field.id !== preferredContact.id)
-    : data.user.profile;
+	const preferredContact = data.user.profile.find(field => field.preferredContact);
+	const contacts = preferredContact
+		? data.user.profile.filter(field => field.id !== preferredContact.id)
+		: data.user.profile;
 
-  return (
-    <div className="pt-5 flex flex-col overflow-hidden">
-      {/* IMG ROUNG LARGE */}
-      {viewingContact && (
-        <div className="absolute pt-20 pl-6">
-          <Icon size={28} type="BACK" onClick={() => navigate('/contacts')} />
-        </div>
-      )}
-      <div className="self-end mt-6 -mr-20">
-        <img
-          className="rounded-full shadow-lg w-96 h-96 object-cover"
-          src={data.user.picture}
-          alt={`profile picuture of ${data.user.name}`}
-        />
-      </div>
-      {/* BLACK TEXT XL-4XL */}
-      <div className="mt-12 mx-6 mb-8">
-        <section>
-          <div className="flex justify-between items-center">
-            <h5 className="text-2xl font-bold">{data.user.name}</h5>
-            {!viewingContact ? (
-              <Link to="edit">
-                {/* EDIT ICON */}
-                <svg
-                  className="mr-3"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M15.4285 4.28564L19.7142 8.57136L8.57136 19.7142H4.28564V15.4285L15.4285 4.28564Z"
-                    stroke="black"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              </Link>
-            ) : (
-                <Icon
-                  size={24}
-                  type="TRASH"
-                  onClick={async () => {
-                    if (deleteLoading) return;
-                    await deleteConnection({
-                      variables: {
-                        id: location.state.connectionId
-                      }
-                    });
-                    navigate('/contacts');
-                  }}
-                />
-              )}
-          </div>
-          <p className="text-gray-700 tracking-wide">{data.user.industry}</p>
-        </section>
-        {preferredContact && (
-          <section className="mt-10">
-            <h2 className="uppercase text-xs text-gray-900 tracking-widest">Preferred Contact</h2>
-            <div className="flex mt-3">
-              <Icon type={preferredContact.type} size={24} />
-              <span className="ml-4 text-blue-500">{preferredContact.value}</span>
-            </div>
-          </section>
-        )}
-        <section className="mt-10">
-          <h2 className="uppercase text-xs text-gray-900 tracking-widest">Contact Methods</h2>
-          <ul className="mt-3">
-            {contacts.length ? contacts.map(field => (
-              <li key={field.id} className="flex mb-3">
-                <Icon type={field.type} size={24} />
-                <span className="ml-4">{field.value}</span>
-              </li>
-            )) : viewingContact ? (
-              <p>They have not shared any other methods of contact.</p>
-            ) : <p>You have not added any other methods of contact.</p>}
-          </ul>
-        </section>
-        <section className="mt-10">
-          <h2 className="uppercase text-xs text-gray-900 tracking-widest">Interests</h2>
-          <div className="flex flex-wrap">
-            <div className="flex items-center bg-gray-200 text-gray-700 rounded-full py-1 px-4 mr-3 mt-3">
-              <span className="text-gray-700 uppercase text-sm tracking-widest">#ai</span>
-            </div>
-            <div className="flex items-center bg-gray-200 text-gray-700 rounded-full py-1 px-4 mr-3 mt-3">
-              <span className="text-gray-700 uppercase text-sm tracking-widest">#fluent</span>
-            </div>
-            <div className="flex items-center bg-gray-200 text-gray-700 rounded-full py-1 px-4 mr-3 mt-3">
-              <span className="text-gray-700 uppercase text-sm tracking-widest">#asia</span>
-            </div>
-            <div className="flex items-center bg-gray-200 text-gray-700 rounded-full py-1 px-4 mr-3 mt-3">
-              <span className="text-gray-700 uppercase text-sm tracking-widest">#bunnies</span>
-            </div>
-            <div className="flex items-center bg-gray-200 text-gray-700 rounded-full py-1 px-4 mr-3 mt-3">
-              <span className="text-gray-700 uppercase text-sm tracking-widest">#kitties</span>
-            </div>
-          </div>
-        </section>
-        <section className="mt-10">
-          <h2 className="uppercase text-xs text-gray-900 tracking-widest">Future Events</h2>
-          <p className="mt-4">None</p>
-        </section>
-        <section className="mt-10">
-          <h2 className="uppercase text-xs text-gray-900 tracking-widest">Past Events</h2>
-          <p className="mt-4">None</p>
-        </section>
-      </div>
-    </div>
-  );
+	return (
+		// following two divs add card style
+		// to profile page
+		<div className="pt-24 pb-6 bg-gray-200">
+			<div className="profile-card pb-4 bg-white mx-6 shadow-md overflow-hidden">
+				<div className="pt-5 flex flex-col overflow-hidden">
+					{/* IMG ROUND LARGE */}
+					{viewingContact && (
+						<div className="absolute pt-20 pl-6">
+							<Icon size={28} type="BACK" onClick={() => navigate('/contacts')} />
+						</div>
+					)}
+					<div className="self-end -mt-16 -mr-12">
+						<img
+							className="rounded-full shadow-lg w-96 h-96 object-cover"
+							src={data.user.picture}
+							alt={`profile picture of ${data.user.name}`}
+						/>
+					</div>
+					{/* BLACK TEXT XL-4XL */}
+					<div className="mt-12 mx-6 mb-8">
+						<section>
+							<div className="flex justify-between items-center">
+								<h5 className="text-2xl font-bold">{data.user.name}</h5>
+								{!viewingContact ? (
+									<Link to="edit">
+										{/* EDIT ICON */}
+										<svg
+											className="mr-3"
+											width="24"
+											height="24"
+											viewBox="0 0 24 24"
+											fill="none"
+											xmlns="http://www.w3.org/2000/svg"
+										>
+											<path
+												d="M15.4285 4.28564L19.7142 8.57136L8.57136 19.7142H4.28564V15.4285L15.4285 4.28564Z"
+												stroke="black"
+												strokeWidth="2"
+												strokeLinecap="round"
+												strokeLinejoin="round"
+											/>
+										</svg>
+									</Link>
+								) : (
+									<Icon
+										size={24}
+										type="TRASH"
+										onClick={async () => {
+											if (deleteLoading) return;
+											await deleteConnection({
+												variables: {
+													id: location.state.connectionId
+												}
+											});
+											navigate('/contacts');
+										}}
+									/>
+								)}
+							</div>
+							<p className="text-gray-700 tracking-wide">{data.user.industry}</p>
+						</section>
+						{preferredContact && (
+							<section className="mt-10">
+								<h2 className="uppercase text-xs text-gray-900 tracking-widest">Preferred Contact</h2>
+								<div className="flex mt-3">
+									<Icon type={preferredContact.type} size={24} />
+									<span className="ml-4 text-blue-500">{preferredContact.value}</span>
+								</div>
+							</section>
+						)}
+						<section className="mt-10">
+							<h2 className="uppercase text-xs text-gray-900 tracking-widest">Contact Methods</h2>
+							<ul className="mt-3">
+								{contacts.length ? (
+									contacts.map(field => (
+										<li key={field.id} className="flex mb-3">
+											<Icon type={field.type} size={24} />
+											<span className="ml-4">{field.value}</span>
+										</li>
+									))
+								) : viewingContact ? (
+									<p>They have not shared any other methods of contact.</p>
+								) : (
+									<p>You have not added any other methods of contact.</p>
+								)}
+							</ul>
+						</section>
+						<section className="mt-10">
+							<h2 className="uppercase text-xs text-gray-900 tracking-widest">Interests</h2>
+							<div className="flex flex-wrap">
+								<div className="flex items-center bg-gray-200 text-gray-700 rounded-full py-1 px-4 mr-3 mt-3">
+									<span className="text-gray-700 uppercase text-sm tracking-widest">#ai</span>
+								</div>
+								<div className="flex items-center bg-gray-200 text-gray-700 rounded-full py-1 px-4 mr-3 mt-3">
+									<span className="text-gray-700 uppercase text-sm tracking-widest">#fluent</span>
+								</div>
+								<div className="flex items-center bg-gray-200 text-gray-700 rounded-full py-1 px-4 mr-3 mt-3">
+									<span className="text-gray-700 uppercase text-sm tracking-widest">#asia</span>
+								</div>
+								<div className="flex items-center bg-gray-200 text-gray-700 rounded-full py-1 px-4 mr-3 mt-3">
+									<span className="text-gray-700 uppercase text-sm tracking-widest">#bunnies</span>
+								</div>
+								<div className="flex items-center bg-gray-200 text-gray-700 rounded-full py-1 px-4 mr-3 mt-3">
+									<span className="text-gray-700 uppercase text-sm tracking-widest">#kitties</span>
+								</div>
+							</div>
+						</section>
+						<section className="mt-10">
+							<h2 className="uppercase text-xs text-gray-900 tracking-widest">Future Events</h2>
+							<p className="mt-4">None</p>
+						</section>
+						<section className="mt-10">
+							<h2 className="uppercase text-xs text-gray-900 tracking-widest">Past Events</h2>
+							<p className="mt-4">None</p>
+						</section>
+					</div>
+				</div>
+				{/* <!-- closing the two card style divs: --> */}
+			</div>
+		</div>
+	);
 };
 
 export default Profile;
