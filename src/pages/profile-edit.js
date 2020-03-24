7d28a87af943ddd781c29261d87fa59e3c57baf5
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-03-24 10:31:15 -0400

    Fixes fixed bug when typing into link input, text goes "haywire" removed "link" from add button for better styling on smaller screens.

--- src/pages/profile-edit.js
+++ src/pages/profile-edit.js
@@ -95,7 +95,7 @@ export default function ProfileEdit(props) {
 
   const handleNewLink = async event => {
     console.log(event);
-    //if (event.key !== 'Enter' || link === 'PLUS') return;
+    if (link === 'PLUS') return;
 
     const profileData = {
       value: fields.link,
@@ -451,7 +451,7 @@ export default function ProfileEdit(props) {
             <svg width="24" height="24" viewBox="0 0 34 35" className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg">
               <path d="M30.2805 6.50024H27.9596C27.6342 6.50024 27.3254 6.64966 27.1262 6.90532L13.4366 24.2473L6.8723 15.9299C6.773 15.8039 6.64643 15.7019 6.50209 15.6318C6.35775 15.5616 6.19939 15.525 6.0389 15.5249H3.71801C3.49554 15.5249 3.37269 15.7805 3.50883 15.9532L12.6032 27.4747C13.0282 28.0125 13.845 28.0125 14.2733 27.4747L30.4897 6.92524C30.6258 6.75591 30.503 6.50024 30.2805 6.50024Z"/>
             </svg>
-              <span> Add Link</span>
+            <span> Add</span>
            
           </button>
           {/* End Link Input */}
