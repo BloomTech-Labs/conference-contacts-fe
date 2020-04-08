e37d8d09b73cc26539a0eb083dd3bbeb110856bc
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-04-08 17:02:25 -0400

    Minor changes

--- src/components/SocialLinks/SocialLinks.js
+++ src/components/SocialLinks/SocialLinks.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 
-
 const SocialLinks = () => {
     return(
         <div>
-            Social Links
+            <Icons/>
+            <Input/> 
         </div>
     );
 }
