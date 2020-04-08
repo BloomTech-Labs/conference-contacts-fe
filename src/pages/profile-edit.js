6ebd6faddfdfda2376b4a15f8ff840839f684901
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-04-08 18:18:18 -0400

    Refactored SocialLinks component

--- src/pages/profile-edit.js
+++ src/pages/profile-edit.js
@@ -12,6 +12,7 @@ import BeatLoader from 'react-spinners/BeatLoader';
 
 // components
 import InputsComponent from '../components/editComponents/inputsComponent';
+import SocialLinks from '../components/SocialLinks/SocialLinks';
 
 export default function ProfileEdit(props) {
   //State
@@ -370,96 +371,15 @@ export default function ProfileEdit(props) {
           )}
         </div>
         {/* Error handling end */}
-        <div className="relative flex items-center">
-          {/* Link Input */}
-          <input
-            type="text"
-            name="link"
-            placeholder="Username or URL"
-            className="w-8/12 border border-gray-900 rounded p-2 pr-10"
-            onChange={handleFieldChange}
-            value={fields.link || ''}
-            //onKeyPress={handleNewLink}
-          />
-          {/* Check Icon Btn */}
-          <button
-            className={`bg-gray-300 hover:text-white text-gray-800 font-bold py-2 px-5 ml-5 rounded inline-flex items-center ${
-              link === 'PLUS' ? 'hover:bg-gray-500 cursor-not-allowed' : 'hover:bg-green-500'
-            }`}
-            onClick={handleNewLink}
-          >
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 34 35"
-              className="fill-current w-4 h-4 mr-2"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M30.2805 6.50024H27.9596C27.6342 6.50024 27.3254 6.64966 27.1262 6.90532L13.4366 24.2473L6.8723 15.9299C6.773 15.8039 6.64643 15.7019 6.50209 15.6318C6.35775 15.5616 6.19939 15.525 6.0389 15.5249H3.71801C3.49554 15.5249 3.37269 15.7805 3.50883 15.9532L12.6032 27.4747C13.0282 28.0125 13.845 28.0125 14.2733 27.4747L30.4897 6.92524C30.6258 6.75591 30.503 6.50024 30.2805 6.50024Z" />
-            </svg>
-            <span> Add</span>
-          </button>
-          {/* End Link Input */}
-          {/* Social Media Icons */}
-          <div id="link-types" className="absolute flex items-center right-1 mr-10 hidden">
-            {['INSTAGRAM', 'LINKEDIN', 'FACEBOOK', 'TWITTER', 'EMAIL'].map(
-              (type) =>
-                type !== link && (
-                  <Icon
-                    id={type}
-                    key={type}
-                    type={type}
-                    size={24}
-                    classes={'social-icons'}
-                    onClick={() => {
-                      setLink(type);
-                      document.getElementById('link-types').classList.toggle('hidden');
-                    }}
-                  />
-                )
-            )}
-          </div>
-          {/* End Social Media Icons */}
-          {/* PLUS Icon within link input */}
-          <Icon
-            type={link}
-            size={24}
-            classes="absolute right-1 mr-3"
-            onClick={() => document.getElementById('link-types').classList.toggle('hidden')}
-          />
-        </div>
+        {/* Social Links Component */}
+        <SocialLinks 
+          handleNewLink={handleNewLink} 
+          fields={fields} 
+          handleFieldChange={handleFieldChange} 
+          link={link}
+          setLink={setLink}
+        />
       </div>
-      {/* End of Link Input Section*/}
-      {/* <div className="mt-4">
-        <div className="flex justify-between items-center">
-          <label className="block text-sm mb-1">Interests</label>
-          <button type="button" className="text-xs text-blue-500 focus:outline-none">
-            &#43; add interest
-          </button>
-        </div>
-        <div className="flex flex-wrap">
-          <div className="flex items-center bg-gray-200 text-gray-700 rounded-full py-1 px-4 mr-3 mt-3">
-            <span className="text-gray-700 uppercase text-sm mr-2 tracking-widest">#ai</span>
-            <Icon type="DELETE" size={14} />
-          </div>
-          <div className="flex items-center bg-gray-200 text-gray-700 rounded-full py-1 px-4 mr-3 mt-3">
-            <span className="text-gray-700 uppercase text-sm mr-2 tracking-widest">#fluent</span>
-            <Icon type="DELETE" size={14} />
-          </div>
-          <div className="flex items-center bg-gray-200 text-gray-700 rounded-full py-1 px-4 mr-3 mt-3">
-            <span className="text-gray-700 uppercase text-sm mr-2 tracking-widest">#asia</span>
-            <Icon type="DELETE" size={14} />
-          </div>
-          <div className="flex items-center bg-gray-200 text-gray-700 rounded-full py-1 px-4 mr-3 mt-3">
-            <span className="text-gray-700 uppercase text-sm mr-2 tracking-widest">#bunnies</span>
-            <Icon type="DELETE" size={14} />
-          </div>
-          <div className="flex items-center bg-gray-200 text-gray-700 rounded-full py-1 px-4 mr-3 mt-3">
-            <span className="text-gray-700 uppercase text-sm mr-2 tracking-widest">#kitties</span>
-            <Icon type="DELETE" size={14} />
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 }
