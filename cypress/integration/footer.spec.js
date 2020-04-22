53c528153258846bdcc8bf56cdaafa28597be26e
Erica L. Ingram <inquiries@aquoco.co>
2020-04-22 11:15:33 -0700

    tested footer background color

--- cypress/integration/footer.spec.js
+++ cypress/integration/footer.spec.js
@@ -13,7 +13,9 @@ context('FOOTER', () => {
 		cy.visit('http://localhost:3000/');
 	});
 	it('contain "Designed by Swaap"', () => {
-		cy.get('footer');
-		cy.get('h5').contains('Copyright ©2020 Designed by Swaap');
+		cy.get('footer').find('h5').contains('Copyright ©2020 Designed by Swaap');
+	});
+	it('checking background color', () => {
+		cy.get('footer').should('have.css', 'background-color').and('eq', 'rgb(155, 105, 228)');
 	});
 });
