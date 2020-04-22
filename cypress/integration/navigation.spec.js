94692a0a12d843b9fc2a691b9faf68bcc7e78e11
Erica L. Ingram <inquiries@aquoco.co>
2020-04-22 05:18:10 -0700

    added four tests to landing page

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,40 +1,35 @@
 /// <reference types="Cypress" />
 
 context('Navigation', () => {
-  before(() => {
-    cy.login()
-      .then(res => res.body)
-      .then(body => {
-        localStorage.setItem('token', body.access_token);
-      });
-    cy.visit('http://localhost:3000/');
-  });
+	before(() => {
+		/* 
+      cy.login()
+        .then(res => res.body)
+        .then(body => {
+          localStorage.setItem('token', body.access_token);
+        });
+    */
+		cy.visit('http://localhost:3000/');
+	});
 
-  it('navigates to contacts page', () => {
-    cy.get('.navContainer')
-      .contains('Contacts')
-      .click();
-    cy.url().should('include', '/contacts');
-  });
+	it('navigates to features page', () => {
+		cy.get('.fill-current').click();
+		cy.get('#menu').contains('Features').click();
+		cy.url().should('include', '/#features');
+	});
 
-  it('navigates to messages page', () => {
-    cy.get('.navContainer')
-      .contains('Messages')
-      .click();
-    cy.url().should('include', '/messages');
-  });
+	it('navigates to demo page', () => {
+		cy.get('#menu').contains('Demo').click();
+		cy.url().should('include', '/#demo');
+	});
 
-  it('navigates to profile page', () => {
-    cy.get('.navContainer')
-      .contains('Profile')
-      .click();
-    cy.url().should('include', '/profile');
-  });
+	it('navigates to reviews page', () => {
+		cy.get('#menu').contains('Reviews').click();
+		cy.url().should('include', '/#reviews');
+	});
 
-  it('navigates to settings page', () => {
-    cy.get('.navContainer')
-      .contains('Settings')
-      .click();
-    cy.url().should('include', '/settings');
-  });
+	it('navigates to login page', () => {
+		cy.get('#menu').contains('Sign Up').click();
+		// cy.url().should('include', '/contacts');
+	});
 });
