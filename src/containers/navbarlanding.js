60a75c5f8c84e721e5616ba8b45d60316f84e01e
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2020-04-27 16:57:44 -0400

    updated landing page UI

--- src/containers/navbarlanding.js
+++ src/containers/navbarlanding.js
@@ -97,7 +97,7 @@ export default function NavBarLanding() {
                 <div>
                   <div className="desktop:px-8 py-3 px-0 flex justify-center">
                     <button
-                      className="bg-blue-500  hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                      className="bg-white-500 hover:text-white hover:bg-blue-500 border-2 border-blue-500 text-blue-500 font-bold py-2 px-4 rounded"
                       onClick={loginWithRedirect}
                     >
                       Login
